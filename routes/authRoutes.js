c85403983af0e5e290e448dd58cdb886c25648b5
Kishan1996singh <kishansingh620@gmail.com>
2024-10-02 15:36:32 +0530

    initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -65,54 +65,10 @@ const router = express.Router();
  *    description: authentication apis
  */
 
-/**
- * @swagger
- * /api/v1/auth/register:
- *    post:
- *      summary: register new user
- *      tags: [Auth]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *      responses:
- *        200:
- *          description: user created successfully
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/components/schemas/User'
- *        500:
- *          description: internal serevr error
- */
 
 // REGISTER || POPST
 router.post("/register", limiter, registerController);
 
-/**
- * @swagger
- * /api/v1/auth/login:
- *  post:
- *    summary: login page
- *    tags: [Auth]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/User'
- *    responses:
- *      200:
- *        description: login successfull
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User'
- *      500:
- *        description: something went wrong
- */
 
 // LOGIN || POST
 router.post("/login", limiter, loginController);
